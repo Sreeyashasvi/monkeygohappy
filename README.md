66133a34c3018da05a779607f76a0b975ca2005e
Sreeyashasvi <74099332+Sreeyashasvi@users.noreply.github.com>
2021-03-09 17:03:39 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# monkeygohappy
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canva;
+var gameState=0;
+var constentCount;
+var database;
+var quiz;
+var question;
+var constent;
+
+function setup(){
+  canvas = createCanvas(1200,900);
+}
+
+
+function draw(){
+  background("white");
+
+  
+}
